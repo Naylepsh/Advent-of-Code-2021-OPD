62749b8766a8d9a2e24821369a494c583ef8fbc8
michal.mikolajczyk <michal.mikolajczyk@irdeto.com>
2023-09-22 15:07:56 +0200

    Task 01

--- aoc-2021-mm/src/sonar_sweep.rs
+++ aoc-2021-mm/src/sonar_sweep.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 
 use crate::BoxedError;
-use aoc_framework::{AocInput, AocSolution, AocTask, traits::*};
+use aoc_framework::{traits::*, AocInput, AocSolution, AocTask};
+use itertools::Itertools;
 
 pub struct SonarSweep;
 
@@ -11,6 +12,24 @@ impl AocTask for SonarSweep {
     }
 
     fn solution(&self, input: AocInput, phase: usize) -> Result<AocSolution, BoxedError> {
-        input.flatten().solved()
+        let window_size = match phase {
+            1 => 1,
+            2 => 3,
+            _ => todo!(),
+        };
+
+        input
+            .flatten()
+            .map(|str| str.parse())
+            .try_collect::<_, Vec<i32>, _>()?
+            .windows(window_size)
+            .fold((0i32, None), |(increases, last), input| {
+                match (last, input.iter().sum::<i32>()) {
+                    (Some(a), b) if b > a => (increases + 1, Some(b)),
+                    (_, b) => (increases, Some(b)),
+                }
+            })
+            .0
+            .solved()
     }
 }

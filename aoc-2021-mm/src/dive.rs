e676606292a422e50532e9f7fa4e16b553bfb435
michal.mikolajczyk <michal.mikolajczyk@irdeto.com>
2023-09-22 17:02:41 +0200

    Task 02

--- aoc-2021-mm/src/dive.rs
+++ aoc-2021-mm/src/dive.rs
@@ -1,9 +1,82 @@
-use std::path::PathBuf;
+use std::{
+    ops::{Add, Mul},
+    path::PathBuf,
+};
 
 use crate::BoxedError;
-use aoc_framework::{AocInput, AocSolution, AocTask, traits::*};
+use aoc_framework::{traits::*, AocInput, AocSolution, AocTask};
+use color_eyre::eyre::{bail, eyre, Context};
+use itertools::Itertools;
 
 pub struct Dive;
+
+#[derive(Default)]
+struct Position {
+    x: i32,
+    y: i32,
+    aim: i32,
+}
+
+impl Position {
+    fn product(self) -> i32 {
+        self.x * self.y
+    }
+}
+
+impl Add<Move> for Position {
+    type Output = Self;
+
+    fn add(mut self, rhs: Move) -> Self::Output {
+        match rhs {
+            Move::Forward(dist) => self.x += dist,
+            Move::Down(dist) => self.y += dist,
+            Move::Up(dist) => self.y -= dist,
+        }
+        self
+    }
+}
+
+impl Mul<Move> for Position {
+    type Output = Position;
+
+    fn mul(mut self, rhs: Move) -> Self::Output {
+        match rhs {
+            Move::Forward(dist) => {
+                self.x += dist;
+                self.y += self.aim * dist
+            }
+            Move::Down(dist) => self.aim += dist,
+            Move::Up(dist) => self.aim -= dist,
+        }
+        self
+    }
+}
+
+enum Move {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+impl TryFrom<String> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let (direction, distance) = value
+            .split(" ")
+            .collect_tuple()
+            .ok_or(eyre!("Invalid Move"))?;
+        let distance: i32 = distance.parse().context("Invalid distance")?;
+
+        let r#move = match direction {
+            "forward" => Self::Forward(distance),
+            "down" => Self::Down(distance),
+            "up" => Self::Up(distance),
+            _ => bail!("Invalid direction"),
+        };
+        Ok(r#move)
+    }
+}
 
 impl AocTask for Dive {
     fn directory(&self) -> PathBuf {
@@ -11,6 +84,17 @@ impl AocTask for Dive {
     }
 
     fn solution(&self, input: AocInput, phase: usize) -> Result<AocSolution, BoxedError> {
-        input.flatten().solved()
+        let op = match phase {
+            1 => Add::add,
+            2 => Mul::mul,
+            _ => todo!(),
+        };
+
+        input
+            .flatten()
+            .map(Move::try_from)
+            .fold_ok(Position::default(), op)?
+            .product()
+            .solved()
     }
 }

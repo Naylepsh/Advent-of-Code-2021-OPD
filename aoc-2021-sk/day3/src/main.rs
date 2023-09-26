5f8a5c71004f7a2155cab3f3e7cb2ae752701212
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-09-26 16:50:21 +0200

    pepela

--- aoc-2021-sk/day3/src/main.rs
+++ aoc-2021-sk/day3/src/main.rs
@@ -8,23 +8,63 @@ fn read_lines(filename: &str) -> Vec<String> {
         .collect()
 }
 
+struct ReadingSummary {
+    zeros: u32,
+    ones: u32,
+}
+
+impl ReadingSummary {
+    pub fn empty() -> ReadingSummary {
+        ReadingSummary { zeros: 0, ones: 0 }
+    }
+
+    pub fn combine(left: ReadingSummary, right: ReadingSummary) -> ReadingSummary {
+        ReadingSummary {
+            zeros: left.zeros + right.zeros,
+            ones: left.ones + right.ones,
+        }
+    }
+
+    pub fn combine_mut(&mut self, other: ReadingSummary) -> () {
+        self.zeros += other.zeros;
+        self.ones += other.ones;
+    }
+}
+
+impl From<Option<char>> for ReadingSummary {
+    fn from(value: Option<char>) -> Self {
+        match value {
+            Some('0') => ReadingSummary { zeros: 1, ones: 0 },
+            Some('1') => ReadingSummary { zeros: 0, ones: 1 },
+            _ => ReadingSummary::empty(),
+        }
+    }
+}
+
+impl From<char> for ReadingSummary {
+    fn from(value: char) -> Self {
+        match value {
+            '0' => ReadingSummary { zeros: 1, ones: 0 },
+            '1' => ReadingSummary { zeros: 0, ones: 1 },
+            _ => ReadingSummary::empty(),
+        }
+    }
+}
+
 fn compute_complex_rating(
     lines: Vec<String>,
     i: usize,
-    determine_bit: fn(i32, i32) -> char,
+    determine_bit: fn(u32, u32) -> char,
 ) -> u32 {
     if lines.len() == 1 {
         isize::from_str_radix(lines[0].as_str(), 2).unwrap() as u32
     } else {
-        let (zeros, ones) =
-            lines
-                .iter()
-                .fold((0, 0), |(zeros, ones), line| match line.chars().nth(i) {
-                    Some('0') => (zeros + 1, ones),
-                    Some('1') => (zeros, ones + 1),
-                    _ => (zeros, ones),
-                });
-        let bit_value = determine_bit(zeros, ones);
+        let summary = lines
+            .iter()
+            .map(|line| ReadingSummary::from(line.chars().nth(i)))
+            .fold(ReadingSummary::empty(), ReadingSummary::combine);
+
+        let bit_value = determine_bit(summary.zeros, summary.ones);
 
         let leftover = lines
             .into_iter()
@@ -35,7 +75,7 @@ fn compute_complex_rating(
     }
 }
 
-fn determine_oxygen_bit(zeros: i32, ones: i32) -> char {
+fn determine_oxygen_bit(zeros: u32, ones: u32) -> char {
     if ones >= zeros {
         '1'
     } else {
@@ -43,7 +83,7 @@ fn determine_oxygen_bit(zeros: i32, ones: i32) -> char {
     }
 }
 
-fn determine_co2_bit(zeros: i32, ones: i32) -> char {
+fn determine_co2_bit(zeros: u32, ones: u32) -> char {
     if ones >= zeros {
         '0'
     } else {
@@ -51,41 +91,36 @@ fn determine_co2_bit(zeros: i32, ones: i32) -> char {
     }
 }
 
-fn solve_first(lines: Vec<String>) -> i32 {
+fn solve_first(lines: Vec<String>) -> u32 {
     let length = lines.first().map(|line| line.len()).unwrap_or(0);
-    let mut counts = (0..length).map(|_| (0, 0)).collect::<Vec<_>>();
+    let mut summaries = (0..length)
+        .map(|_| ReadingSummary::empty())
+        .collect::<Vec<_>>();
+
     lines.iter().for_each(|line| {
         line.chars().enumerate().for_each(|(i, bit)| {
-            let (zeros, ones) = counts[i];
-            let result = match bit {
-                '0' => (zeros + 1, ones),
-                '1' => (zeros, ones + 1),
-                _ => (zeros, ones),
-            };
-            counts[i] = result;
+            summaries[i].combine_mut(ReadingSummary::from(bit));
         })
     });
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
-    counts.iter().for_each(|(ones, zeros)| {
-        if ones > zeros {
-            gamma = (gamma << 1) + 1;
-            epsilon <<= 1;
+    let (gamma, epsilon) = summaries.iter().fold((0, 0), |(gamma, epsilon), summary| {
+        if summary.ones > summary.zeros {
+            ((gamma << 1) + 1, epsilon << 1)
         } else {
-            gamma <<= 1;
-            epsilon = (epsilon << 1) + 1;
-        };
+            (gamma << 1, (epsilon << 1) + 1)
+        }
     });
 
     gamma * epsilon
 }
 
 fn solve_second(lines: Vec<String>) -> u32 {
-    let oxy = compute_complex_rating(lines.clone(), 0, determine_oxygen_bit);
+    let oxygen = compute_complex_rating(lines.clone(), 0, determine_oxygen_bit);
     let co2 = compute_complex_rating(lines, 0, determine_co2_bit);
 
-    oxy * co2
+    println!("O:{} CO2:{}", oxygen, co2);
+
+    oxygen * co2
 }
 
 fn main() {

11b20b901249c765f307d47087d8b084b221779e
naylepsh <naylepsh@gmail.com>
2023-10-20 20:29:54 +0200

    cleanup

--- aoc-2021-sk/day6/day6.ml
+++ aoc-2021-sk/day6/day6.ml
@@ -1,25 +1,4 @@
 module Lanternfish = struct
-  let new_fish = 8
-  let reset_fish = 6
-
-  let rec tick fishes old_fishes new_fishes =
-    match fishes with
-    | [] -> List.rev old_fishes @ List.rev new_fishes
-    | fish :: rest ->
-        if fish == 0 then
-          tick rest (reset_fish :: old_fishes) (new_fish :: new_fishes)
-        else tick rest ((fish - 1) :: old_fishes) new_fishes
-
-  let tick fishes = tick fishes [] []
-
-  let rec tick_for n fishes =
-    if n <= 0 then fishes else tick_for (n - 1) (tick fishes)
-
-  let show fishes = String.concat "," (List.map string_of_int fishes)
-  let from_string str = List.map int_of_string (String.split_on_char ',' str)
-end
-
-module LF = struct
   let tick fishes =
     Array.mapi
       (fun index _ ->
@@ -53,7 +32,7 @@ let read_lines file =
 let () =
   match read_lines "day6.input" with
   | line :: [] ->
-      let fishes = LF.from_string line in
-      let fs = LF.tick_for 256 fishes in
-      print_endline (string_of_int (LF.count fs))
+      let fishes = Lanternfish.from_string line in
+      let fs = Lanternfish.tick_for 256 fishes in
+      print_endline (string_of_int (Lanternfish.count fs))
   | _ -> print_endline "?"
